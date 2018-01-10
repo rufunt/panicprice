34224975245c0a5fc2d2e64c9f5ad081951d0ed7
rufunt <jazzoo@mail.ru>
2018-01-11 02:27:58 +0300

    add styling for listing products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+Product.create!(title: 'Ruby', description: "Ruby is the fastest dynamic language", image_url: 'ruby.jpg', price: 55.0)
